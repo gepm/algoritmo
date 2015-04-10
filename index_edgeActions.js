502f0e35a8efee837b53a58b4caf07b63608f8f2
gepm <castanedavisual@gmail.com>
2015-04-10 03:31:43 -0500

    nuevos

---
+++ index_edgeActions.js
@@ -0,0 +1,180 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_inicio}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2748,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",4250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_1}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_home_4}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_2}","click",function(sym,e){sym.play("alg2");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_2}","click",function(sym,e){sym.play("home");sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_2}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_2}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",5750,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_3}","click",function(sym,e){sym.play("alg3");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",7250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",8753,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_4}","click",function(sym,e){sym.play("alg4");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_5}","click",function(sym,e){sym.play("alg5");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",11750,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_6}","click",function(sym,e){sym.play("alg6");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_micta_h}","click",function(sym,e){sym.play("mictasol");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",14641,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",17644,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_tav_h}","click",function(sym,e){sym.play("tavor");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",20418,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_btn_aso_h}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",23425,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_btn_tea_h}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",26500,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_comb_h}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_next_h}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_mic}","click",function(sym,e){sym.play("mictasol");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_tav}","click",function(sym,e){sym.play("tavor");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_aso}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_tea}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_e_com}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_1}","click",function(sym,e){sym.play("alg2");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_2_micta}","click",function(sym,e){sym.play("tavor");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_2_tav}","click",function(sym,e){sym.play("mictasol");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_2}","click",function(sym,e){sym.play("alg3");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_3}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_3}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_3_aso}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_3_tea}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_3_com}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_3}","click",function(sym,e){sym.play("alg2");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_3}","click",function(sym,e){sym.play("alg4");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_4}","click",function(sym,e){sym.play("alg5");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_4}","click",function(sym,e){sym.play("alg3");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_4}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_4}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_4_aso}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_4_tea}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_4_com}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_5}","click",function(sym,e){sym.play("alg6");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_5}","click",function(sym,e){sym.play("alg4");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_5}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_5}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_5_aso}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_5_tea}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_5_com}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_6}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_6}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_6_aso}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_6_tea}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_bt_6_com}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_6}","click",function(sym,e){sym.play("mictasol");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_6}","click",function(sym,e){sym.play("alg5");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_micta}","click",function(sym,e){sym.play("tavor");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_micta}","click",function(sym,e){sym.play("alg6");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_mic}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_mic}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_tav}","click",function(sym,e){sym.play("mictasol");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_tav}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_tav}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_tav}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_aso}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_aso}","click",function(sym,e){sym.play("tavor");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_aso}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_aso}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_tea}","click",function(sym,e){sym.play("comb");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_tea}","click",function(sym,e){sym.play("asoflon");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_tea}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_tea}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_ant_comb}","click",function(sym,e){sym.play("tealep");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_next_comb}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_home_comb}","click",function(sym,e){sym.play("home");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_b_alg_comb}","click",function(sym,e){sym.play("alg1");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})(jQuery,AdobeEdge,"EDGE-1689744");
